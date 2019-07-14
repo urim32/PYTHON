6d213842804bc09b7da3014fdd10c71dda852898
uri malasa <urimalasa30@gmail.com>
2019-07-14 12:23:06 +0300

    added

---
+++ Python Environment/star ex1 Built-in_Modules/star_ex1.py
@@ -0,0 +1,16 @@
+# 1
+# math, operator ,time , numbers , json ,
+# i used the function help and pass it 'modules' and it print all the built module
+# 2
+import math
+import operator
+import time
+from math import sin, sqrt, tan
+from time import *
+from math import factorial as calcFactorial
+from math import exp as exponent
+from math import ceil as ceiling
+
+assert(calcFactorial(5) == 120)
+print(exponent(3))
+print(ceiling(3.2))
